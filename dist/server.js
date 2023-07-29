fe79b8792804869eba42d1e2464c8981a4163660
Cn <xieny2902w@gmail.com>
2023-07-29 22:37:50 +0800

    feat: bind port

--- dist/server.js
+++ dist/server.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const GET_OBJECT = {
     hello: "world"
 };
